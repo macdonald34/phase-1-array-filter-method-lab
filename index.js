fff44e57f7bbe57794a1462fe6bf1bbd3e91a5d6
macdonald34 <144123866+macdonald34@users.noreply.github.com>
2023-10-02 09:27:03 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Code your solution here
+function findMatching(drivers, name){
+    return drivers.filter(drivers=>drivers.toLowerCase()===name.toLowerCase());
+}
+function fuzzyMatch(drivers,Initials){
+    return drivers.filter(drivers=>drivers.startsWith(Initials));
+}
+function matchName(drivers,name){
+    return drivers.filter(drivers=>drivers.name===name);
+}
